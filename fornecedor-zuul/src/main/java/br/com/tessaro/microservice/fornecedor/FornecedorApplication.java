585ad75871ee9716d4b0ab3cef1d4bfcf07fc5d1
cassioate <cassioate@gmail.com>
2021-02-23 07:46:02 -0300

    base para microservice com zull e eureka

---
+++ fornecedor-zuul/src/main/java/br/com/tessaro/microservice/fornecedor/FornecedorApplication.java
@@ -0,0 +1,16 @@
+package br.com.tessaro.microservice.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FornecedorApplication {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.cloud.bootstrap.enabled","true");
+		SpringApplication.run(FornecedorApplication.class, args);
+	}
+
+}
